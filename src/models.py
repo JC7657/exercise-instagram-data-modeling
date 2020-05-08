5dab70d1817fb9cd89479b0bfee1591897d291bd
JC7657 <juan.jpjs@gmail.com>
2020-05-08 07:22:53 +0000

    Finished

--- src/models.py
+++ src/models.py
@@ -1,30 +1,53 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    bio = Column(String(250))
+    posts = relationship('Post', backref='user', lazy=True)
+    stories = relationship('Story')
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    image = Column(String(250), nullable=False)
+    likes = Column(Integer, nullable=False)
+    description = Column(String(250))
+    comments = relationship('Comment')
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable=False)
+    likes = Column(Integer)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+class Story(Base):
+    __tablename__ = 'stories'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    creation_time = Column(DateTime, default=datetime.now(), nullable=False)
+    views = Column(Integer, nullable=False)
+    isHighlighted = Column(Boolean, default=False)
+    
 
     def to_dict(self):
         return {}
